e89eb1fbc7fb6efa12d8db1760c48359b0e31f8c
Despite Gfx <despitegfx@gmail.com>
2019-07-26 13:18:33 +0100

    Final update, Global Code '19 Project

--- myapp.py
+++ myapp.py
@@ -1,63 +1,55 @@
-
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template, url_for, request, redirect
 app = Flask(__name__)
 import pyowm
-from gpiozero import LED
-from time import sleep
+from datetime import datetime
+#from gpiozero import LED             
+#from time import sleep
+                                                                                                                                 
+@app.route('/')                                                   
+def index():                                   
+    return render_template('home.html')                                                         
+                                                             
 
-@app.route('/')
-def index():
-
-    location = 'London, UK'
-    apikey = '51c6723dd2f51626dd33896729e79676'
-    owm = pyowm.OWM(apikey)
-    observation = owm.weather_at_place(location)
-    w = observation.get_weather()
-
-    
-
-#    winds = w.get_wind()
-#    humidities = w.get_humidity()
-    tempreture = w.get_temperature()
-#    presh = w.get_pressure()
-#    clud = w.get_clouds()
-#    ran = w.get_rain()
-#    snow = w.get_snow()
-    tem = tempreture
-    
-
-
-    return render_template('home.html', index = tem)
-
-
-
-@app.route("/weather")
+@app.route("/weather")                                                                                                                              
 def weather():
     return render_template('weather.html')
 
-
-@app.route("/weather/submit")
+                                                            
+@app.route("/weather/submit", methods=['GET', 'POST'])
 def submit():
-    
-    location = 'London, UK'
+    user_input_1=request.form.get('city')
+    user_input_2=request.form.get('country')
+        
+    place = user_input_1 + ", " + user_input_2                                         
+    print(place)
+
     apikey = '51c6723dd2f51626dd33896729e79676'
     owm = pyowm.OWM(apikey)
-    observation = owm.weather_at_place(location)
+    observation = owm.weather_at_place(place)
     w = observation.get_weather()
 
-    
-
-#    winds = w.get_wind()
-#    humidities = w.get_humidity()
+    winds = w.get_wind()
+    humidities = w.get_humidity()
     tempreture = w.get_temperature()
-#    presh = w.get_pressure()
-#    clud = w.get_clouds()
-#    ran = w.get_rain()
-#    snow = w.get_snow()
-    tem = tempreture
+    presh = w.get_pressure()
+    clud = w.get_clouds()
+    ran = w.get_rain()
+    snow = w.get_snow()
+    sr = w.get_sunrise_time('iso')
+    ss = w.get_sunset_time('iso')
+    tm = w.get_reference_time(timeformat='iso')
+    
+    
+    tem = {"City": user_input_1, "Country": user_input_2, "Wind" : winds, "Humidity" : humidities, "Temperature" : tempreture,
+           "Pressure" : presh, "Cloud" : clud, "Rain" : ran, "Snow" : snow, "Sun Rise":sr, "Sun Set": ss, "Time Referenced": tm }
+    
+    
     return render_template('submit.html', index = tem)
-
+ 
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')              
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
